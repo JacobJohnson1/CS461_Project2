09a626d1084e1a8231b47d8014782b37acc1e068
Jacob Johnson <jdjhc4@umsystem.edu>
2022-03-05 12:09:06 -0600

    initial commit

---
+++ driver.py
@@ -0,0 +1,39 @@
+import fileReader
+import random
+
+maxWeight = 500
+
+def initialTemp():
+    #no idea how to initialize this
+    T = 40000
+    return T
+
+def initialSolution(listOfItems):
+    numOfItems = 20
+    initialScore = 0
+    for i in range(0, 20):
+        listOfItems[random.randint(0, 200)][2] = 1
+
+def calcUtility(listOfItems):
+    totalUtility = 0.0
+    for i in range(0, len(listOfItems)):
+        if listOfItems[i][2] == 1:
+            print(listOfItems[i])
+            totalUtility += listOfItems[i][0]
+    return(totalUtility)
+
+
+def calcWeight(listOfItems):
+    totalWeight = 0.0
+    for i in range(0, len(listOfItems)):
+        if listOfItems[i][2] == 1:
+            totalWeight += listOfItems[i][1]
+    return(totalWeight)
+    
+def driver():
+    listOfItems = fileReader.formatInput()
+    initialSolution(listOfItems)
+    initialUtility = calcUtility(listOfItems)
+    initialWeight = calcWeight(listOfItems)
+
+driver()
---
+++ fileReader.py
@@ -0,0 +1,17 @@
+import os
+
+def formatInput():
+    inputFile = open('Program2Input.txt', 'r')
+    inputArray = inputFile.read()
+    inputArray = inputArray.split()
+    nestedInputArray = []
+
+
+
+    nestedInputArray = [inputArray[i:i+2] for i in range(0, len(inputArray), 2)]
+    nestedInputArray = [[float(x) for x in lst] for lst in nestedInputArray]
+
+    for i in range(0, len(nestedInputArray)):
+        nestedInputArray[i].append(0)
+
+    return(nestedInputArray)
