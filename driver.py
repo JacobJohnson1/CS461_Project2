212d491f631e9253861fe4519dcab27f3db7dd53
Jacob Johnson <jdjhc4@umsystem.edu>
2022-03-05 19:04:12 -0600

    overweight penalty function created

--- driver.py
+++ driver.py
@@ -18,7 +18,6 @@ def calcUtility(listOfItems):
     totalUtility = 0.0
     for i in range(0, len(listOfItems)):
         if listOfItems[i][2] == 1:
-            print(listOfItems[i])
             totalUtility += listOfItems[i][0]
     return(totalUtility)
 
@@ -30,6 +29,12 @@ def calcWeight(listOfItems):
             totalWeight += listOfItems[i][1]
     return(totalWeight)
     
+def overWeightCheck(utility, weight):
+    if weight > maxWeight:
+        penalty = ((weight - maxWeight) * 20)
+        utility -= penalty
+    return utility
+
 def driver():
     listOfItems = fileReader.formatInput()
     initialSolution(listOfItems)
